0bf84be1f5e4d59c45267f0d8251255373c0f4b2
Dylan <102.23.dylan.chan@gmail.com>
2024-09-14 22:25:03 +0800

    Update main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 int main() {
     cout << "Hello World" << "\n";
+    string input = "";
+
+    while (true) {
+        if (input == "bye") {
+            cout << "bye bye!!\n";
+            break;
+        }
+
+        cin >> input;
+        cout << "You said: " << input << "\n";
+    }
+
     return 0;
 }
