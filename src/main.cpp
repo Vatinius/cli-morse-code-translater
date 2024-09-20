7d8b0833df3936f559542412c78364540ea3bae6
Vatinius <102.23.dylan.chan@gmail.com>
2024-09-20 15:34:04 +0800

    Complete first version

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,71 @@
 #include <iostream>
 #include <string.h>
-
 using namespace std;
 
-int main() {
-    cout << "Hello World" << "\n";
-    string input = "";
-
-    while (true) {
-        if (input == "bye") {
-            cout << "bye bye!!\n";
-            break;
-        }
-
-        cin >> input;
-        cout << "You said: " << input << "\n";
+string translate(char character) {
+    switch (character) {
+        case 'A': case 'a': return ".-";
+        case 'B': case 'b': return "-...";
+        case 'C': case 'c': return "-.-.";
+        case 'D': case 'd': return "-..";
+        case 'E': case 'e': return ".";
+        case 'F': case 'f': return "..-.";
+        case 'G': case 'g': return "--.";
+        case 'H': case 'h': return "....";
+        case 'I': case 'i': return "..";
+        case 'J': case 'j': return ".---";
+        case 'K': case 'k': return "-.-";
+        case 'L': case 'l': return ".-..";
+        case 'M': case 'm': return "--";
+        case 'N': case 'n': return "-.";
+        case 'O': case 'o': return "---";
+        case 'P': case 'p': return ".--.";
+        case 'Q': case 'q': return "--.-";
+        case 'R': case 'r': return ".-.";
+        case 'S': case 's': return "...";
+        case 'T': case 't': return "-";
+        case 'U': case 'u': return "..-";
+        case 'V': case 'v': return "...-";
+        case 'W': case 'w': return ".--";
+        case 'X': case 'x': return "-..-";
+        case 'Y': case 'y': return "-.--";
+        case 'Z': case 'z': return "--..";
+        case '1': return ".----";
+        case '2': return "..---";
+        case '3': return "...--";
+        case '4': return "....-";
+        case '5': return ".....";
+        case '6': return "-....";
+        case '7': return "--...";
+        case '8': return "---..";
+        case '9': return "----.";
+        case '0': return "-----";
+        default: return "";
     }
+}
+
+string parser(string text) {
+    string result = "";
+
+    for (int i = 0; i < text.length(); i++) {
+        result += translate(text.at(i));    
+    }
+
+    return result;
+}
+
+int main() {
+    string text;
+
+    cout << "Hello! What text do you want to translate? \n\n";
+    cin >> text;
+
+    while (text != "/bye") {
+        cout << "In morse code, it is: " << parser(text) << "\n\n";
+        cout << "What else do you want to translate? \n";
+        cin >> text;
+    }
+
 
     return 0;
 }
